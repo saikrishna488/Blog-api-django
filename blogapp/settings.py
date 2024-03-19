b134db6d2ddd23e4acdafad01261fc50b05b857a
saikrishna488 <oltvez@gmail.com>
2024-03-19 16:15:50 +0530

    update 3

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -122,12 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'public/'
-# MEDIA_URL = "images/"
+MEDIA_URL = "images/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "images",
 ]
 STATIC_ROOT = BASE_DIR/'staticfiles'
+MEDIA_ROOT = BASE_DIR 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
